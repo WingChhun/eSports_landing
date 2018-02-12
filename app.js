4a30d1020f9672b84a52952b42aa067f7ec58f3d
WingChhun <chhunjamesdc2@yahoo.com>
2018-02-11 18:17:04 -0800

    added csgo routes and api routes

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 const express = require('express'),
     app = express(),
+
     bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
-app.use(bodyParser.json);
+const apiRoutes = require("./routes/api.js");
+const csRoutes = require("./routes/csgo.js");
+//APP SETTINGS
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+//app.set('view engine', 'ejs'); //default template ejs
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/src'));
+//api routes
 
-app.set(express.static(__dirname + '/views'));
-
-app.set(express.static(__dirname + '/src'));
-
-//include routes here
-
-
-//root route
-app.get("/", (req, res) => {
-    res.render("index.html");
-
-
+app.use("/api", apiRoutes);
+app.use("/csgo", csRoutes);
+//ROUTES
+app.get('/', (req, res) => {
+    res.render("src/index.html");
 });
 
-app.get("*", (req, res) => {
-    res.redirect("/");
-})
 
-//start servver
+
+
+
+
 app.listen(port, () => {
-    console.log("Server has started, listening on port... " + port);
+    console.log("Server has started on port " + port);
 });

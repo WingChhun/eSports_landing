7611331b4c5ae9b73921ee3aec4db9828bc2c528
WingChhun <chhunjamesdc2@yahoo.com>
2018-02-16 22:44:41 -0800

    changed src/js output to src/js/vendors

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('sass', () => {
 
 gulp.task('js', () => {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js'])
-        .pipe(gulp.dest('src/js'))
+        .pipe(gulp.dest('src/js/vendors'))
         .pipe(browserSync.stream());
 });
 
